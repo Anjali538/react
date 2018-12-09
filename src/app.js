77e512b789e81dccb03c7e3870938e6f0618a1e2
unknown <anjalisingh538@gmail.com>
2018-12-09 21:55:04 +0530

    Configration done w/o create react app

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import styles from '../styles/app.css';
+
+class App extends Component {
+	render() {
+		return <div className={styles.style}>Hello Anjali</div>;
+	}
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+const htmlPlugins = new HtmlWebpackPlugin({
+    template: './public/index.html',
+    filename: 'index.html'
+})
+
+const BundlePlugin = new BundleAnalyzerPlugin({
+    generateStatsFile: true,
+    analyzerMode: 'disabled',
+})
+
+module.exports = {
+    'entry': './src/index.js',
+    'output': {
+        path: path.resolve(__dirname, './dist'),
+        filename: '[name].bundle.js'
+    },
+    resolve: {
+        extensions: [".js",".jsx",".css"]
+    },
+    devServer: {
+        inline: true,
+        publicPath: '/',
+        contentBase: './dist'
+    },
+    module:  {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true,
+                            localIdentName: '[name]__[local]__[hash:base64:8]',
+                            sourceMap: true,
+                            importLoaders: 1,
+                            url: true
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'html-loader',
+                        options: {
+                            minimize: true
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [htmlPlugins, BundlePlugin]
+}
